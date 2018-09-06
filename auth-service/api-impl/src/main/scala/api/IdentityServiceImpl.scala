fe2afd0ead8ed525d3f6869a8ac651f8fe853431
Damian Fauth <dfauth@gmail.com>
2018-09-07 07:44:48 +0800

    added roles to TokenContent

--- auth-service/api-impl/src/main/scala/api/IdentityServiceImpl.scala
+++ auth-service/api-impl/src/main/scala/api/IdentityServiceImpl.scala
@@ -1,15 +1,42 @@
 package api
 
 import akka.NotUsed
+import api.authentication.TokenContent
 import api.request.{ClientRegistration, UserCreation, UserLogin}
 import api.response.{GeneratedIdDone, TokenRefreshDone, UserLoginDone}
+import api.validation.ValidationUtil.validate
 import com.lightbend.lagom.scaladsl.api.ServiceCall
+import com.lightbend.lagom.scaladsl.api.transport.Forbidden
 
 class IdentityServiceImpl extends IdentityService {
 
   override def registerClient(): ServiceCall[ClientRegistration, GeneratedIdDone] = ???
 
   override def loginUser(): ServiceCall[UserLogin, UserLoginDone] = ???
+
+//  override def loginUser() = ServiceCall { request =>
+//    def passwordMatches(providedPassword: String, storedHashedPassword: String) = SecurePasswordHashing.validatePassword(providedPassword, storedHashedPassword)
+//
+//    validate(request)
+//
+//    for {
+//      maybeUser <- identityRepository.findUserByUsername(request.username)
+//
+//      token = maybeUser.filter(user => passwordMatches(request.password, user.hashedPassword))
+//        .map(user =>
+//          TokenContent(
+//            clientId = user.clientId,
+//            userId = user.id,
+//            username = user.username
+//          )
+//        )
+//        .map(tokenContent => JwtTokenUtil.generateTokens(tokenContent))
+//        .getOrElse(throw Forbidden("Username and password combination not found"))
+//    }
+//      yield {
+//        UserLoginDone(token.authToken, token.refreshToken.getOrElse(throw new IllegalStateException("Refresh token missing")))
+//      }
+//  }
 
   override def refreshToken(): ServiceCall[NotUsed, TokenRefreshDone] = ???
 
--- test-service/api-impl/src/main/scala/api/IdentityServiceImpl.scala
+++ test-service/api-impl/src/main/scala/api/IdentityServiceImpl.scala
@@ -1,19 +1,10 @@
 package api
 
-import java.util.UUID
-
 import api.authentication.AuthenticationServiceComposition._
-import api.authentication.TokenContent
-//import api.request.WithUserCreationFields
-//import api.response.{TokenRefreshDone, UserLoginDone}
-import api.validation.ValidationUtil._
-import com.lightbend.lagom.scaladsl.api.ServiceCall
-import com.lightbend.lagom.scaladsl.api.transport.{BadRequest, Forbidden}
 import com.lightbend.lagom.scaladsl.persistence.PersistentEntityRegistry
 import com.lightbend.lagom.scaladsl.server.ServerServiceCall
-//import io.digitalcat.publictransportation.services.identity.impl.util.{JwtTokenUtil, SecurePasswordHashing}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 class IdentityServiceImpl(
                            persistentRegistry: PersistentEntityRegistry
