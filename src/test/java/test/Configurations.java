9831af1caafe27d63a2b479afeabf892dd10d426
Damian Fauth <dfauth@gmail.com>
2018-10-07 10:51:42 +0800

    automat tests

---
+++ src/test/java/test/Configurations.java
@@ -0,0 +1,29 @@
+package test;
+
+import automat.Automat;
+import automat.AutomationContext;
+
+import java.util.function.Function;
+
+import static automat.Automat.Utils.forHttpCode;
+import static automat.Functions.*;
+//import static test.TestResource$.MODULE$.*;
+
+public class Configurations {
+
+    public static Function<AutomationContext, Automat> basicClientWithWebSocket = c -> c.onRequest().
+            apply(authHandler).
+            onResponse().
+            apply(
+                    forHttpCode(403).
+                            use(
+                                    loginHandler(TestResource$.MODULE$.AUTH())
+                                            .andThen(storeToken)
+                                            .andThen(subscribeTo(
+                                                    TestResource$.MODULE$.SUBSCRIPTION(),
+                                                    heartbeatConsumer(c.queue())
+                                                    ) // subscribeTo
+                                            ) // andThen
+                            ) // use
+            );// apply;
+}
