053feaf0283a01e76082f4ee9b8d599476ce1e9d
fauthd <damian.fauth@sgx.com>
2018-12-17 10:18:02 +0800

    refactor: PriviledgeActionRunner -> CallableRunner

--- authzn/src/main/java/thingy/CallableRunner.java
+++ authzn/src/main/java/thingy/CallableRunner.java
@@ -10,18 +10,6 @@ public interface CallableRunner {
 
     <R> R run(Callable<R> callable) throws SecurityException;
 
-//    default <R> R run(PrivilegedAction<R> action) throws SecurityException {
-//        return run(adapter(action));
-//    }
-//
-//    default <R> R run(Supplier<R> supplier) throws SecurityException {
-//        return run(adapter(supplier));
-//    }
-//
-//    default <R> R run(Function<Void, R> f) throws SecurityException {
-//        return run(adapter(f));
-//    }
-
     static <R> Callable<R> adapter(PrivilegedAction<R> action) {
         return () -> action.run();
     }
